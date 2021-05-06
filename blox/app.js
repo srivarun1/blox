78ac5e44babedc5665890b57457b450c3ec8dd0e
Srivarun <nsrivarun@gmail.com>
2021-05-07 01:00:29 +0530

    Chane=ges in app.js

--- blox/app.js
+++ blox/app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var authRouter = require('./routes/auth');
+var createUserRouter = require('./routes/createUser');
+var postRouter = require('./routes/post');
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/authenticate', authRouter);
+app.use('/createuser', createUserRouter);
+app.use('/post', postRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
